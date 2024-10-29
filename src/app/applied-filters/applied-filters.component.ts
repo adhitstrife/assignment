57f7cfbcbc4cb08d038bd8d81926a1a6361f9611
adhitstrife63 <adhitstrife63@gmail.com>
2024-10-29 15:39:21 +0000

    applied filter

--- src/app/applied-filters/applied-filters.component.ts
+++ src/app/applied-filters/applied-filters.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ChipModule } from 'primeng/chip';
 
 @Component({
   selector: 'app-applied-filters',
   standalone: true,
-  imports: [],
+  imports: [
+    ChipModule
+  ],
   templateUrl: './applied-filters.component.html',
   styleUrl: './applied-filters.component.css'
 })
